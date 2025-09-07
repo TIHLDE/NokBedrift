08ee3cb2d24484e5ba11009cc0efa3156608965c
Mori <anders.morille@gmail.com>
2025-09-07 16:19:10 +0200

    Moved from Photon

---
+++ src/types/ContentType.tsx
@@ -0,0 +1,3 @@
+import type { Event } from '@/types/Event';
+import type { News } from '@/types/News';
+export type ContentType = Event | News;
---
+++ src/types/Order.tsx
@@ -0,0 +1,10 @@
+import type { Event } from '@/types/Event';
+import type { User } from '@/types/User';
+
+export type Order = {
+  order_id: string;
+  user_id: User['user_id'];
+  event: Event['id'];
+  status: string;
+  payment_link: string;
+};
