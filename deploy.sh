4b7868bf81d8ba91659aad815147ee6b43ed6991
Borgar Barland <142639383+borgaar@users.noreply.github.com>
2025-10-07 00:06:38 +0200

    updated deployment

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,25 @@
 
 set -e
 
+################################
+# CONFIGURATION
+# Change these values as needed
+DOMAIN="bedrift.tihlde.org"
+PORT=3500
+ENV_FILE_PATH=".env"
+################################
+
+COMMIT_HASH=$(git rev-parse --short HEAD)
+IMAGE_NAME="$DOMAIN:$COMMIT_HASH"
+
 echo "-> Loading env variables from .env file into shell"
 export $(grep -v '^#' .env | xargs)
 
-COMMIT_HASH=$(git rev-parse --short HEAD)
-
 echo "-> Building new Docker image"
-docker build --no-cache --build-arg NEXT_PUBLIC_TIHLDE_API_URL=$NEXT_PUBLIC_TIHLDE_API_URL -t bedrift.tihlde.org:$COMMIT_HASH .
+docker build --build-arg NEXT_PUBLIC_ALLOWED_GROUP_SLUGS=$NEXT_PUBLIC_ALLOWED_GROUP_SLUGS --no-cache -t $IMAGE_NAME .
 
 echo "-> Stopping and removing old container"
-docker rm -f bedrift.tihlde.org
+docker rm -f $DOMAIN || true
 
 echo "-> Starting new container"
-docker run --env-file .env -p 3500:3000 --name bedrift.tihlde.org --restart unless-stopped -d bedrift.tihlde.org:$COMMIT_HASH
-
+docker run --env-file $ENV_FILE_PATH -p $PORT:3000 --name $DOMAIN --restart unless-stopped -d $IMAGE_NAME
